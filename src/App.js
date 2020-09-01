890234222775494419643562da87721d727648b6
duracell27 <volodymyrshmidt@gmail.com>
2020-09-02 00:37:41 +0300

    first button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {
+  Button
+} from './components';
+
 
 function App() {
-  return (
-    <div className="wrapper">
-        hello, world
-    </div>
+  return ( 
+    <div className = "wrapper" >
+      <Button size='large' type={'primary'}> This is button </Button>
+    </div >
   );
 }
 
